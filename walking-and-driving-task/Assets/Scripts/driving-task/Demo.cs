9126c832c85c09f9baa0225b06f752b3a499904d
kash-patel <ksp.sdd.ptl@gmail.com>
2021-05-27 22:44:00 -0400

    Add basic acceleration and car interior model

--- walking-and-driving-task/Assets/Scripts/driving-task/Demo.cs
+++ walking-and-driving-task/Assets/Scripts/driving-task/Demo.cs
@@ -2,17 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Demo : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+public class Demo : MonoBehaviour {
 
-    // Update is called once per frame
-    void Update()
-    {
-        transform.Translate(Vector3.forward * Time.deltaTime);
-    }
+	float topSpeed = 16.0f;
+	float acceleration = 4.0f;
+
+	Rigidbody rb = null;
+
+
+	// Start is called before the first frame update
+	void Start () {
+
+		rb = GetComponent<Rigidbody>();
+
+		if (rb) Debug.Log("Got the rigidbody.");
+		else Debug.Log("No rigidbody.");
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		rb.velocity = rb.velocity.z < topSpeed ? Vector3.forward * (rb.velocity.z + acceleration * Time.deltaTime) : Vector3.forward * topSpeed;
+	}
 }
